9249c9c0ef5b5b9d1fbee88c2f4ab5ebd15b511e
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-28 10:47:03 -0400

    moved files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.css';
-import App from './components/App.js';
-
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
