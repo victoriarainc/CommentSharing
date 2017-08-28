8b8edeb9d84c026d6ee7ea65a838040607b7003c
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-28 10:49:43 -0400

    fixed imports (app is running)

--- commentsharing/src/components/App.js
+++ commentsharing/src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
-import BaseLayout from '../components/Baselayout.js';
-import ParentComponent from '../components/Parentcomponent.js';
+import BaseLayout from './Baselayout.js';
+import ParentComponent from './Parentcomponent.js';
 
 class App extends Component {
   render() {
