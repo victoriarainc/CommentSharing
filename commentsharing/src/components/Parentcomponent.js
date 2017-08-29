9de6863f5a953517d8665bed791f55a4b5516157
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-28 21:24:01 -0400

    fixed ParentComponent

--- commentsharing/src/components/Parentcomponent.js
+++ commentsharing/src/components/Parentcomponent.js
@@ -5,30 +5,29 @@ import DisplayComponent from '../components/Displaycomponent.js';
 class ParentComponent extends Component {
   constructor(props) {
     super(props);
-    //state lives here
+  
     this.state = {
       whatToSay: "",
       whatWasSaid: ""
     }
-    //we are really in a *bind* here.... :)
-    //fix it...
+
     this.handleInput = this.handleInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
   }
   handleInput(e) {
     e.preventDefault();
-    //set the state on input change
+    //set state on input change
     let whatToSay = e.target.value
     this.setState({whatToSay: whatToSay});
   }
   handleSubmit(e) {
     e.preventDefault();
-    //check console to see if firing
+    //check console if firing
     console.log("fired");
-    //set the state for props and for value (prevents output from appearing when typing)
+    //set the state
     this.setState({whatToSay: this.state.whatToSay, whatWasSaid: this.state.whatToSay});
-    //clear our input by resetting state
+    //clear the input
     this.setState({whatToSay: ""});
 
   }
